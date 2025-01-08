e05b5106ead0b7b407adf706811bf0254bf864d0
Vedant Kesarwani <78657126+InventedSarawak@users.noreply.github.com>
2025-01-09 02:48:11 +0530

    Time: 0 ms (100%), Space: 17.8 MB (15.62%) - LeetHub

---
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.py
@@ -0,0 +1,16 @@
+class Solution:
+    def check(self, nums: List[int]) -> bool:
+        length: int = len(nums)
+        isNotSorted: int = 0
+        j: int = 0
+        for j in range(length):  # j == 3
+            i: int = 0
+            while i < length - 1:
+                if nums[(i + j) % length] > nums[(i + j + 1) % length]:
+                    isNotSorted = 1
+                    break
+                i += 1
+            if not isNotSorted:
+                return True
+            isNotSorted = 0
+        return False
