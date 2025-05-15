4d65e999a809a2eaf94e3c5a86c1df64c4edcb7b
Vedant Kesarwani <78657126+InventedSarawak@users.noreply.github.com>
2025-05-15 14:19:55 +0530

    Time: 0 ms (100%), Space: 19.5 MB (94.56%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,7 +1,6 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        single: list[int] = []
+        xor: int = 0
         for num in nums:
-            if num in single: single.remove(num)
-            else: single.append(num)
-        return single[0]
+            xor ^= num
+        return xor
