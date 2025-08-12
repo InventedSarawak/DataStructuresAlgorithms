a70e97750cdf056b573eb9f435471186fc51a118
Vedant Kesarwani <78657126+InventedSarawak@users.noreply.github.com>
2025-08-12 21:54:21 +0530

    Time: 11 ms (72.33%), Space: 17.7 MB (97.54%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,21 +1,15 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        freq_1 = {}
-        freq_2 = {}
-
-        if len(s) != len(t):
+        freq = [0] * 26
+        len_s = len(s)
+        len_t = len(t) 
+        if len_s != len_t:
             return False
-        for i in range(len(s)):
-            if s[i] in freq_1:
-                freq_1[s[i]] += 1
-            else:
-                freq_1[s[i]] = 1
-            
-            if t[i] in freq_2:
-                freq_2[t[i]] += 1
-            else:
-                freq_2[t[i]] = 1
+        for i in range(len_s):
+            freq[ord(s[i]) - ord('a')] += 1
+            freq[ord(t[i]) - ord('a')] -= 1
         
-        if freq_1 == freq_2:
-            return True
-        return False
+        for i in freq:
+            if i != 0:
+                return False
+        return True
