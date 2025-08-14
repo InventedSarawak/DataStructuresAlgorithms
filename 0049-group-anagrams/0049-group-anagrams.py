9718249e0c5de7837650d7400d9fb4b39f1bc795
Vedant Kesarwani <78657126+InventedSarawak@users.noreply.github.com>
2025-08-14 16:24:27 +0530

    Time: 22 ms (19.94%), Space: 22.7 MB (12.18%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,11 +1,25 @@
-class Solution:
+# class Solution:
 
+#     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+#         hash = {}
+#         for string in strs:
+#             sorted_str = ''.join(sorted(string))
+#             if sorted_str in hash:
+#                 hash[sorted_str].append(string)
+#             else:
+#                 hash[sorted_str] = [string]
+#         return list(hash.values())
+
+from collections import defaultdict
+
+class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        hash = {}
+        hash_map = defaultdict(list)
         for string in strs:
-            sorted_str = ''.join(sorted(string))
-            if sorted_str in hash:
-                hash[sorted_str].append(string)
-            else:
-                hash[sorted_str] = [string]
-        return list(hash.values())
+            # Count characters (only 26 lowercase English letters)
+            count = [0] * 26
+            for char in string:
+                count[ord(char) - ord('a')] += 1
+            # Use tuple of counts as key
+            hash_map[tuple(count)].append(string)
+        return list(hash_map.values())
