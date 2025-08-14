506f431eaaa6b6455a8b51caab24acc493affdf4
Vedant Kesarwani <78657126+InventedSarawak@users.noreply.github.com>
2025-08-14 16:16:26 +0530

    Time: 10 ms (86.8%), Space: 20.7 MB (76.41%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -2,10 +2,10 @@ class Solution:
 
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         hash = {}
-        for idx, string in enumerate(strs):
+        for string in strs:
             sorted_str = ''.join(sorted(string))
             if sorted_str in hash:
-                hash[sorted_str].append(strs[idx])
+                hash[sorted_str].append(string)
             else:
-                hash[sorted_str] = [strs[idx]]
+                hash[sorted_str] = [string]
         return list(hash.values())
